ec1115975a87720763987d07f9665ae0d275afd4
Ntwari30 <jonathanishimwe98@gmail.com>
2022-12-12 09:07:33 -0800

    My first C program that prints a string of characters using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a string of characters using puts
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
