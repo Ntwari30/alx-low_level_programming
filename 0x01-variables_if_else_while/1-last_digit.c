82cd67dac835d13a5ffd7a6f18c3fdb993f339f2
Ntwari30 <jonathanishimwe98@gmail.com>
2022-12-19 07:03:33 -0800

    C  gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,6 +20,6 @@ int main(void)
 	else if (lst_digit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, lst_digit);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, st_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lst_digit);
 	return (0);
 }
